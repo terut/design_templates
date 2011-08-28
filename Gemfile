923bf8d703f9132486e851876673b3ecf63eae34
terut <terut.dev+github@gmail.com>
2011-08-28 22:36:19 +0900

    Edit index.

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@
 source "http://rubygems.org"
 
 gem 'sinatra'
+gem 'sinatra-reloader'
 gem 'slim'
 gem 'sass'
--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@
 require 'bundler'
 Bundler.require(:default)
 
+configure(:development) do |c|
+  require 'sinatra/reloader'
+  c.also_reload '*.rb'
+end
+
 get '/style.css' do
   content_type 'text/css', :charset => 'utf-8'
   scss :style
